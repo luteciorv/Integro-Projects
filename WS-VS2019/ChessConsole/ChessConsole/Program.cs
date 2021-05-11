0bdf6fe33e93393f1f9815ce456132f055e189ce
Teemote <luteciorv@gmail.com>
2021-05-11 16:23:42 -0300

    ChessConsole => Visual novo do Tabuleiro e Mudanca de cor na exibicao das pecas

--- WS-VS2019/ChessConsole/ChessConsole/Program.cs
+++ WS-VS2019/ChessConsole/ChessConsole/Program.cs
@@ -8,10 +8,23 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            ChessPosition position = new ChessPosition('C', 7);
-            Console.WriteLine(position);
+            try
+            {
+                Board board = new Board(8, 8);
 
-            Console.WriteLine(position.ConvertPosition());
+                board.PutPiece(new Tower(board, Color.Yellow), new Position(0, 0));
+                board.PutPiece(new Tower(board, Color.Yellow), new Position(2, 5));
+                board.PutPiece(new King(board, Color.White), new Position(3, 0));
+                board.PutPiece(new King(board, Color.White), new Position(3, 7));
+
+
+                Screen.DisplayBoard(board);
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- WS-VS2019/ChessConsole/ChessConsole/Screen.cs
+++ WS-VS2019/ChessConsole/ChessConsole/Screen.cs
@@ -7,21 +7,66 @@ namespace ChessConsole
     {
         public static void DisplayBoard(Board board)
         {
+            // Pular 2 linhas
+            Console.WriteLine("\n###### CHESS CONSOLE ######\n");
+
             // Percorrer as linhas do tabuleiro
             for(int row = 0; row < board.Rows; row++)
             {
+                // Exibir a linha
+                Console.Write($"  {8 - row}   ");
+
                 // Percorrer as colunas do tabuleiro
                 for(int column = 0; column < board.Columns; column++)
                 {
-                    // Decidir o que exibir
-                    string display = board.GetPiece(row, column) == null ? "- " : board.GetPiece(row, column) + " ";
-                    
-                    Console.Write(display);
+                    // Caso nenhuma peça tenha sido encontrada
+                    if (board.GetPiece(row, column) == null)
+                    {
+                        Console.Write("- ");
+                    }
+
+                    // Caso alguma peça tenha sido encontrada
+                    else
+                    {
+                        DisplayPiece(board.GetPiece(row, column));
+
+                        Console.Write(" ");
+                    }                                       
                 }
 
                 // Pular uma linha
                 Console.WriteLine("");
             }
+
+            // Exibir a linha
+            Console.WriteLine("\n      A B C D E F G H");
+        }
+
+        public static void DisplayPiece(Piece piece)
+        {
+            // Cor atual do console
+            ConsoleColor normalColor = Console.ForegroundColor;
+
+            // Cor a ser usada
+            Console.ForegroundColor = ConsoleColor.White;
+
+            switch (piece.Color)
+            {
+                // Amarelo
+                case (Color.Yellow):                    
+                    Console.ForegroundColor = ConsoleColor.Yellow;                                        
+                    break;
+
+                // Nenhuma peça encontrada
+                default:
+                    break;
+            }
+            
+            // Exibir a peça
+            Console.Write(piece);
+
+            // Voltar ao normal
+            Console.ForegroundColor = normalColor;
         }
     }
 }
